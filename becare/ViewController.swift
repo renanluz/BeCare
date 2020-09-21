2191e881406beebfbc839e8c773128cee7c4037b
Renanluz <ren4n@live.com>
2020-09-21 08:58:16 -0300

    Initial Commit

---
+++ becare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  becare
+//
+//  Created by Renan Luz on 21/09/20.
+//  Copyright © 2020 RenanLuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
